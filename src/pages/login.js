45908c78dcdf942c66f184be4a92e54580a542d0
codepilot777 <codepilot777@gmail.com>
2022-08-23 22:08:38 +0800

    configured tailwind config, added form component for the login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import FirebaseContext from '../contexts/firebase';
-
 
 const Login = () => {
   const navigate = useNavigate(); 
@@ -11,7 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const isInvalid = password === '' || emailAddress === '';
+  const isInvalid = (password === '' || emailAddress === '');
   
   const handleLogin = () => {
     
@@ -27,15 +26,25 @@ const Login = () => {
         <img src="/images/iphone-with-profile.jpeg" alt="iPhone background" />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1>
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
-        </h1>
-        { error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-        <form onSubmit={handleLogin} method="POST">
-          <input arial-label="Enter your email address" type="text" placeholder="Email address" className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" onChange={() => {}}/>
-          <input arial-label="Enter your password" type="password" placeholder="Password" className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" onChange={() => {}}/>
-          <button disable={isInvalid} type="submit" className="bg-blue-medium text-white">Log In</button>
-        </form>
+        <div className="flex flex-col tiems-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1>
+            <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          </h1>
+          { error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input arial-label="Enter your email address" type="text" placeholder="Email address" className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" onChange={(e) => setEmailAddress(e.target.value)}/>
+            <input arial-label="Enter your password" type="password" placeholder="Password" className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" onChange={(e) => setPassword(e.target.value)}/>
+            <button disable={isInvalid} type="submit" className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${isInvalid && "opacity-50"}`}>Log In</button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
+          <p className="text-sm">
+            Don't have an account? {' '}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      white: '#fffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    },
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    })
   },
   plugins: [],
 }
