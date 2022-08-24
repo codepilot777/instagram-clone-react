d3be48fbb3835d71cf730e8db5887a9484bf3cd0
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-08-24 15:15:23 +0800

    commit

--- src/pages/login.js
+++ src/pages/login.js
@@ -22,19 +22,19 @@ const Login = () => {
   }, []);
   
   return (
-    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+    <div className="container flex items-center h-screen max-w-screen-md mx-auto">
       <div className="flex w-3/5">
         <img src="/images/iphone-with-profile.jpeg" alt="iPhone background" />
       </div>
       <div className="flex flex-col w-2/5">
         <h1>
-          <img src="/images/logo.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+          <img src="/images/logo.png" alt="Instagram" className="w-6/12 mt-2 mb-4" />
         </h1>
         { error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
         <form onSubmit={handleLogin} method="POST">
-          <input arial-label="Enter your email address" type="text" placeholder="Email address" className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" onChange={() => {}}/>
-          <input arial-label="Enter your password" type="password" placeholder="Password" className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2" onChange={() => {}}/>
-          <button disable={isInvalid} type="submit" className="bg-blue-medium text-white">Log In</button>
+          <input arial-label="Enter your email address" type="text" placeholder="Email address" className="w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary" onChange={() => {}}/>
+          <input arial-label="Enter your password" type="password" placeholder="Password" className="w-full h-2 px-4 py-5 mb-2 mr-3 text-sm border rounded text-gray-base border-gray-primary" onChange={() => {}}/>
+          <button disable={isInvalid} type="submit" className="text-white bg-blue-medium">Log In</button>
         </form>
       </div>
     </div>
